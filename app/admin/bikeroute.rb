217067f7ba3800ff1bd0fa2d612f2dcf8c983e86
thfield <thfield@gmail.com>
2015-06-15 10:24:09 -0700

    ActiveAdmin functional

--- app/admin/bikeroute.rb
+++ app/admin/bikeroute.rb
@@ -28,4 +28,30 @@ ActiveAdmin.register Bikeroute do
   end
 
 
+  show do
+    attributes_table do
+      row :name
+      row :description
+      row :lane_type
+      row :grade
+      row :distance
+      row :neighborhoods do |bikeroute|
+        bikeroute.neighborhoods.map{ |neighborhood| neighborhood.name }.join(', ')
+      end
+    end
+    #active_admin_comments
+  end
+
+
+  form do |f|
+    f.inputs "Route Details" do
+      f.input :name
+      f.input :description
+      f.input :lane_type
+      f.input :grade
+      f.input :distance
+      f.input :neighborhoods, :as => :check_boxes
+    end
+    f.actions
+  end
 end
--- app/admin/neighborhood.rb
+++ app/admin/neighborhood.rb
@@ -12,6 +12,37 @@ ActiveAdmin.register Neighborhood do
 #   permitted << :other if resource.something?
 #   permitted
 # end
+  permit_params :name, :map, bikeroutes: [:id, :name]
 
+  index do
+    column :name
+    column :map
+    column :bikeroutes do |neighborhood|
+      neighborhood.bikeroutes.map{ |bikeroute| bikeroute.name }.join(', ')
+    end
+    actions
+  end
+
+
+  show do
+    attributes_table do
+      row :name
+      row :map
+      row :bikeroutes do |neighborhood|
+        neighborhood.bikeroutes.map{ |bikeroute| bikeroute.name }.join(', ')
+      end
+    end
+    #active_admin_comments
+  end
+
+
+  form do |f|
+    f.inputs "Route Details" do
+      f.input :name
+      f.input :map
+      f.input :bikeroutes, :as => :check_boxes
+    end
+    f.actions
+  end
 
 end
