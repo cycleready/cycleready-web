c7eb54a5c82d4282901f6f64e6f08f110c88226f
James Youn <jamesyoun710@gmail.com>
2015-10-07 21:15:20 -0700

    Config files for test environment

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,32 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+require 'minitest/rails'
+
+require 'capybara/rails'
+require 'minitest/rails/capybara'
+
+Minitest::Reporters.use!(
+  Minitest::Reporters::ProgressReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
+
+# for creating sessions in tests
+include Warden::Test::Helpers
+Warden.test_mode!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
+
+class ActionController::TestCase
+  include Devise::TestHelpers
+end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+end
+
