2abaf391e58ca4aa23ecb2d724c06a8e431b1886
thfield <thfield@gmail.com>
2015-11-17 10:49:50 -0800

    maybe fix loading issue

--- app/assets/javascripts/neighborhoodMap.js
+++ app/assets/javascripts/neighborhoodMap.js
@@ -1,4 +1,5 @@
-var dataPath = '/data/sf-neighborhoods.json';
+var dataPath = '/data/planning_neighborhoods_simple.topojson';
+// var dataPath = '/data/sf-neighborhoods.json';
 
 
 var margin = {top: 10, left: 10, bottom: 10, right: 10}
