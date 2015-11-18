753921517e13f0f5cd30790cee3388ff2d10047b
thfield <thfield@gmail.com>
2015-11-17 23:34:57 -0800

    uncomment lines

--- app/assets/javascripts/bikeroutes.js
+++ app/assets/javascripts/bikeroutes.js
@@ -1,21 +1,21 @@
-// L.mapbox.accessToken = accessToken;
+L.mapbox.accessToken = accessToken;
+
+var map = L.mapbox.map('map', 'mapbox.run-bike-hike')
+    .setView([37.778313, -122.416105], 14);
 //
-// var map = L.mapbox.map('map', 'mapbox.run-bike-hike')
-//     .setView([37.778313, -122.416105], 14);
-// //
-// var bikeRoute = map.featureLayer.setGeoJSON(routegeojson, {
-//   style: function (feature) {
-//     return {
-//       color: feature.properties.stroke,
-//       weight: feature.properties['stroke-width'],
-//       opacity: feature.properties['stroke-opacity']
-//     };
-// 	}
-// });
-// bikeRoute.eachLayer( function (layer){
-//   map.setView([layer.feature.centroid[1], layer.feature.centroid[0]], 14 );
-//   layer.bindPopup(layer.feature.properties.name);
-// });
+var bikeRoute = map.featureLayer.setGeoJSON(routegeojson, {
+  style: function (feature) {
+    return {
+      color: feature.properties.stroke,
+      weight: feature.properties['stroke-width'],
+      opacity: feature.properties['stroke-opacity']
+    };
+	}
+});
+bikeRoute.eachLayer( function (layer){
+  map.setView([layer.feature.centroid[1], layer.feature.centroid[0]], 14 );
+  layer.bindPopup(layer.feature.properties.name);
+});
 
 
 var profileEl = d3.select("#elev");
