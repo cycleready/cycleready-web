aace192c97dc5cee6d1f2d0538a50e694ae8d18c
thfield <thfield@gmail.com>
2015-11-27 16:29:44 -0800

    mapbox route styling

--- app/assets/javascripts/bikeroutes.js
+++ app/assets/javascripts/bikeroutes.js
@@ -1,17 +1,12 @@
 L.mapbox.accessToken = accessToken;
 
+routegeojson.properties.stroke = '#465037';
+routegeojson.properties['stroke-width'] = '5';
+
 var map = L.mapbox.map('map', 'mapbox.run-bike-hike')
     .setView([37.778313, -122.416105], 14);
 //
-var bikeRoute = map.featureLayer.setGeoJSON(routegeojson, {
-  style: function (feature) {
-    return {
-      color: feature.properties.stroke,
-      weight: feature.properties['stroke-width'],
-      opacity: feature.properties['stroke-opacity']
-    };
-	}
-});
+var bikeRoute = map.featureLayer.setGeoJSON(routegeojson);
 bikeRoute.eachLayer( function (layer){
   map.setView([layer.feature.centroid[1], layer.feature.centroid[0]], 14 );
   layer.bindPopup(layer.feature.properties.name);
